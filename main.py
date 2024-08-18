04ce69791e49ff9e879ed427dd26d816e65c704a
Sundarakrishnan <sundarakrishnan.ganesh@gmail.com>
2024-08-18 13:49:43 +0200

    removed polling finally

--- main.py
+++ main.py
@@ -71,8 +71,7 @@ def main() -> None:
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
     # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-    
+    # application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == "__main__":
