b256b2c72de7bea42fd21f2a057df111f5c55980
Sundarakrishnan <sundarakrishnan.ganesh@gmail.com>
2024-08-21 14:19:08 +0200

    main.py implementation update

--- main.py
+++ main.py
@@ -67,39 +67,12 @@ async def main() -> None:
     # Pass webhook settings to telegram
     await application.bot.set_webhook(url=f"{WEBHOOK_URL}/telegram", allowed_updates=Update.ALL_TYPES)
 
-    @app.route("/telegram", methods=["POST"])  # type: ignore[misc]
+    @app.route("/", methods=["POST"])  # type: ignore[misc]
     async def telegram() -> Response:
         """Handle incoming Telegram updates by putting them into the `update_queue`"""
         await application.update_queue.put(Update.de_json(data=request.json, bot=application.bot))
         return Response(status=HTTPStatus.OK)
-
-    @app.route("/submitpayload", methods=["GET", "POST"])  # type: ignore[misc]
-    async def custom_updates() -> Response:
-        """
-        Handle incoming webhook updates by also putting them into the `update_queue` if
-        the required parameters were passed correctly.
-        """
-        try:
-            user_id = int(request.args["user_id"])
-            payload = request.args["payload"]
-        except KeyError:
-            abort(
-                HTTPStatus.BAD_REQUEST,
-                "Please pass both `user_id` and `payload` as query parameters.",
-            )
-        except ValueError:
-            abort(HTTPStatus.BAD_REQUEST, "The `user_id` must be a string!")
-
-        await application.update_queue.put(WebhookUpdate(user_id=user_id, payload=payload))
-        return Response(status=HTTPStatus.OK)
-
-    @app.route("/healthcheck", , methods=["GET"])  # type: ignore[misc]
-    async def health() -> Response:
-        """For the health endpoint, reply with a simple plain text message."""
-        response = make_response("The bot is still running fine :)", HTTPStatus.OK)
-        response.mimetype = "text/plain"
-        return response
-
+    
     webserver = uvicorn.Server(
         config=uvicorn.Config(
             app=WsgiToAsgi(app),
